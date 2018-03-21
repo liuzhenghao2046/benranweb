e2e057549dbf9c8597661e5b3a7296c34c7a8e3e
li_jerry <lijerrymagic@gmail.com>
2018-03-20 23:38:25 -0400

    first attempt
    add index.html

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.use(function (req, res, next){
+    console.log("HTTP request", req.method, req.url, req.body);
+    return next();
+});
+
+app.get('/', (req, res) => res.redirect('/index.html'))
+app.get('/about', (req, res) => res.redirect('/about.html'))
+// app.get('/', function (req, res, next) {
+//     return res.redirect('/index.html');
+// });
+app.use(express.static('frontend'));
+
+var http = require('http');
+
+// http.createServer(app).listen(process.env.PORT, function () {
+//     console.log('HTTP on port');
+http.createServer(app).listen(3000, function () {
+    console.log('HTTP on port 3000');
+});
